f52fbcce283544715b8fcbe9fe3de38d56cb5764
teleskop150750 <teleskop150750@gmail.com>
2024-09-16 10:37:02 +0300

    wip

---
+++ packages/vue-primitives/src/dropdown-menu/stories/DropdownMenu.stories.tsx
@@ -0,0 +1,12 @@
+import CModality from './Modality.vue'
+import CStyled from './Styled.vue'
+
+export default { title: 'Components/DropdownMenu' }
+
+export function Styled() {
+  return CStyled
+}
+
+export function Modality() {
+  return CModality
+}

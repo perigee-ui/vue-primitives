e705d6411118988c8b9169f0e971a5b2b6c0e6f4
teleskop150750 <teleskop150750@gmail.com>
2024-10-06 23:39:57 +0300

    wip: fix dialog useFocusScope

--- packages/vue-primitives/src/focus-scope/FocusScope.ts
+++ packages/vue-primitives/src/focus-scope/FocusScope.ts
@@ -41,10 +41,12 @@ export const AUTOFOCUS_ON_UNMOUNT = 'focusScope.autoFocusOnUnmount'
 export interface UseFocusScopeProps extends EmitsToHookProps<FocusScopeEmits> {
   el?: Ref<HTMLElement | undefined>
   loop?: boolean
-  trapped?: boolean
+  trapped?: () => boolean
 }
 
 export function useFocusScope(props: UseFocusScopeProps): RadixPrimitiveReturns {
+  const { trapped = () => false } = props
+
   const el = props.el || shallowRef<HTMLElement>()
   const setTemplateEl = props.el ? undefined : (value: HTMLElement | undefined) => el.value = value
 
@@ -117,7 +119,7 @@ export function useFocusScope(props: UseFocusScopeProps): RadixPrimitiveReturns 
     }
 
     watchEffect(() => {
-      if (!props.trapped)
+      if (!trapped())
         return
 
       document.addEventListener('focusin', handleFocusIn)
@@ -188,7 +190,7 @@ export function useFocusScope(props: UseFocusScopeProps): RadixPrimitiveReturns 
 
   // Takes care of looping focus (when tabbing whilst at the edges)
   function onKeydown(event: KeyboardEvent) {
-    if (!props.loop && !toValue(props.trapped))
+    if (!props.loop && !trapped())
       return
     if (focusScope.paused)
       return

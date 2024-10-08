161ce3679a6ce964ce674b8fc9ea88e3656c1df1
teleskop150750 <teleskop150750@gmail.com>
2024-10-08 14:26:55 +0300

    wip: hoverCard

--- packages/vue-primitives/src/hover-card/HoverCardTrigger.ts
+++ packages/vue-primitives/src/hover-card/HoverCardTrigger.ts
@@ -1,15 +1,82 @@
-import type { PopperAnchorProps } from '../popper'
-import type { PrimitiveProps } from '../primitive'
+import type { PrimitiveProps } from '../primitive/index.ts'
+import type { RadixPrimitiveReturns } from '../shared/index.ts'
+import { onMounted } from 'vue'
+import { useRef } from '../hooks/index.ts'
+import { type PopperAnchorProps, usePopperContext } from '../popper/index.ts'
+import { mergePrimitiveAttrs } from '../shared/mergeProps.ts'
+import { useHoverCardContext } from './HoverCardRoot.ts'
 
 export interface HoverCardTriggerProps extends PopperAnchorProps {
   as?: PrimitiveProps['as']
 }
 
-// eslint-disable-next-line ts/consistent-type-definitions
-export type HoverCardTriggerEmits = {
-  pointerenter: [event: PointerEvent]
-  pointerleave: [event: PointerEvent]
-  focus: [event: FocusEvent]
-  blur: [event: FocusEvent]
-  touchstart: [event: TouchEvent]
+export function useHoverCardTrigger(): RadixPrimitiveReturns {
+  const context = useHoverCardContext('HoverCardTrigger')
+
+  function onPointerenter(event: PointerEvent) {
+    if (event.defaultPrevented)
+      return
+    if (event.pointerType === 'touch')
+      return
+    context.onOpen()
+  }
+
+  // function onPointerleave(event: PointerEvent) {
+  //   if (event.defaultPrevented)
+  //     return
+  //   if (event.pointerType === 'touch')
+  //     return
+  //   context.onClose()
+  // }
+
+  function onFocus(event: FocusEvent) {
+    if (event.defaultPrevented)
+      return
+    context.onOpen()
+  }
+
+  function onBlur(event: FocusEvent) {
+    if (event.defaultPrevented)
+      return
+    context.onClose()
+  }
+
+  // prevent focus event on touch devices
+  function onTouchstart(event: TouchEvent) {
+    if (event.defaultPrevented)
+      return
+    event.preventDefault()
+  }
+
+  // COMP::PopperAnchor
+
+  const popperContext = usePopperContext('HoverCardTrigger')
+
+  const el = useRef<HTMLElement>()
+  function setTemplateEl(v: HTMLElement | undefined) {
+    el.value = v
+  }
+
+  onMounted(() => {
+    popperContext.onAnchorChange(el.value)
+  })
+
+  return {
+    attrs(extraAttrs) {
+      const attrs = {
+        elRef: setTemplateEl,
+        onPointerenter,
+        // onPointerleave,
+        onFocus,
+        onBlur,
+        onTouchstart,
+      }
+
+      if (extraAttrs) {
+        mergePrimitiveAttrs(attrs, extraAttrs)
+      }
+
+      return attrs
+    },
+  }
 }

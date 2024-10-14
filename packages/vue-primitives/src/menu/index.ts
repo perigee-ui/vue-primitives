a704d3f5665d4d56bacfc22fe253f6b089a02bf3
teleskop150750 <teleskop150750@gmail.com>
2024-10-14 09:08:56 +0300

    ref: Menu exports

--- packages/vue-primitives/src/menu/index.ts
+++ packages/vue-primitives/src/menu/index.ts
@@ -5,24 +5,49 @@ export { Primitive as MenuLabel } from '../primitive/index.ts'
 export {
   type MenuCheckboxItemEmits,
   type MenuCheckboxItemProps,
+  useMenuCheckboxItem,
+  type UseMenuCheckboxItemProps,
 } from './MenuCheckboxItem.ts'
 export { default as MenuCheckboxItem } from './MenuCheckboxItem.vue'
 export {
   type MenuContentProps,
+  useMenuContent,
+  type UseMenuContentProps,
 } from './MenuContent.ts'
 export { default as MenuContent } from './MenuContent.vue'
+export {
+  type MenuContentContext,
+  type MenuContentImplEmits,
+  type MenuContentImplProps,
+  provideMenuContentContext,
+  useMenuContentContext,
+  useMenuContentImpl,
+  useMenuContentImplModal,
+  useMenuContentImplNonModal,
+  type UseMenuContentImplProps,
+  useMenuContentImplShared,
+  type UseMenuContentImplSharedEmits,
+  type UseMenuContentImplSharedPeturns,
+  type UseMenuContentImplSharedPrivateEmits,
+  type UseMenuContentImplSharedPrivateProps,
+  type UseMenuContentImplSharedProps,
+} from './MenuContentImpl.ts'
 export { default as MenuGroup } from './MenuGroup.vue'
 export {
   ITEM_SELECT,
   type MenuItemEmits,
   type MenuItemProps,
+  useMenuItem,
+  type UseMenuItemProps,
 } from './MenuItem.ts'
 export { default as MenuItem } from './MenuItem.vue'
 export {
-  type ItemIndicatorContext as CheckboxContext,
+  type ItemIndicatorContext,
   type MenuItemIndicatorProps,
   provideItemIndicatorContext,
   useItemIndicatorContext,
+  useMenuItemIndicator,
+  type UseMenuItemIndicatorProps,
 } from './MenuItemIndicator.ts'
 export { default as MenuItemIndicator } from './MenuItemIndicator.vue'
 export {
@@ -30,11 +55,16 @@ export {
   type MenuRadioGroupEmits,
   type MenuRadioGroupProps,
   providRadioGroupContext,
+  useMenuRadioGroup,
+  type UseMenuRadioGroupProps,
   useRadioGroupContext,
 } from './MenuRadioGroup.ts'
 export { default as MenuRadioGroup } from './MenuRadioGroup.vue'
 export {
+  type MenuRadioItemEmits,
   type MenuRadioItemProps,
+  useMenuRadioItem,
+  type UseMenuRadioItemProps,
 } from './MenuRadioItem.ts'
 
 export { default as MenuRadioItem } from './MenuRadioItem.vue'
@@ -46,9 +76,10 @@ export {
   type MenuRootProps,
   provideMenuContext,
   provideMenuRootContext,
-  useIsUsingKeyboard,
   useMenuContext,
+  useMenuRoot,
   useMenuRootContext,
+  type UseMenuRootProps,
 } from './MenuRoot.ts'
 
 export { default as MenuRoot } from './MenuRoot.vue'
@@ -60,19 +91,31 @@ export {
   type MenuSubEmits,
   type MenuSubProps,
   provideMenuSubContext,
+  useMenuSub,
   useMenuSubContext,
+  type UseMenuSubProps,
 } from './MenuSub.ts'
 
 export { default as MenuSub } from './MenuSub.vue'
+
+export {
+  type MenuSubContentProps,
+  useMenuSubContent,
+  type UseMenuSubContentProps,
+} from './MenuSubContent.ts'
 
 export { default as MenuSubContent } from './MenuSubContent.vue'
 
 export {
   type MenuSubContentImplEmits,
   type MenuSubContentImplProps,
+  useMenuSubContentImpl,
+  type UseMenuSubContentImplProps,
 } from './MenuSubContentImpl.ts'
 
 export {
   type MenuSubTriggerProps,
+  useMenuSubTrigger,
+  type UseMenuSubTriggerProps,
 } from './MenuSubTrigger.ts'
 export { default as MenuSubTrigger } from './MenuSubTrigger.vue'
